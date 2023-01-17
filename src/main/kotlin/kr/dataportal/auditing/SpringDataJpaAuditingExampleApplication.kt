3d3bf0f29793bcf1270f8812439a296e2d045486
Taeyang Jin (Heli) <sun@dataportal.kr>
2023-01-18 01:40:09 +0900

    chore: initial commit

---
+++ src/main/kotlin/kr/dataportal/auditing/SpringDataJpaAuditingExampleApplication.kt
@@ -0,0 +1,11 @@
+package kr.dataportal.auditing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDataJpaAuditingExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringDataJpaAuditingExampleApplication>(*args)
+}
---
+++ src/test/kotlin/kr/dataportal/auditing/SpringDataJpaAuditingExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.dataportal.auditing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDataJpaAuditingExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
